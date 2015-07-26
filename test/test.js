bb43b2e58434a90d8a172890be7c9497ed3701b8
Ricky Reusser <rsreusser@gmail.com>
2015-07-26 17:56:27 -0400

    Cleanup

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
-'use strict';
+'use strict'
 
-var euler = require('../lib');
-var assert = require('chai').assert;
+var euler = require('../lib')
+var assert = require('chai').assert
 
 
 var ctors = {
@@ -15,44 +15,44 @@ Object.keys(ctors).forEach(function(dtype) {
 
   describe('euler integration (' + dtype + ')', function() {
 
-    var integrator, f, y0, t0;
+    var integrator, f, y0, t0
 
     beforeEach(function() {
       f = function(dydt, y) {
-        dydt[0] = -y[1];
-        dydt[1] =  y[0];
-      };
+        dydt[0] = -y[1]
+        dydt[1] =  y[0]
+      }
 
       t0 = 1.5
-      y0 = new ctor([1,0]);
+      y0 = new ctor([1,0])
 
-      integrator = euler( y0, f, t0, 1 );
-    });
+      integrator = euler( y0, f, t0, 1 )
+    })
 
     it("takes a single timestep",function() {
-      integrator.step();
-      assert.closeTo( integrator.y[0], 1, 1e-4 );
-      assert.closeTo( integrator.y[1], 1, 1e-4 );
-    });
+      integrator.step()
+      assert.closeTo( integrator.y[0], 1, 1e-4 )
+      assert.closeTo( integrator.y[1], 1, 1e-4 )
+    })
 
     it("increments the time",function() {
-      integrator.step();
-      assert.closeTo( integrator.t, t0 + integrator.dt, 1e-4 );
-      integrator.step();
-      assert.closeTo( integrator.t, t0 + 2*integrator.dt, 1e-4 );
-    });
+      integrator.step()
+      assert.closeTo( integrator.t, t0 + integrator.dt, 1e-4 )
+      integrator.step()
+      assert.closeTo( integrator.t, t0 + 2*integrator.dt, 1e-4 )
+    })
 
     it("takes multiple timesteps",function() {
-      integrator.steps(2);
-      assert.closeTo(integrator.y[0], 0, 1e-4 );
-      assert.closeTo(integrator.y[1], 2, 1e-4);
-    });
+      integrator.steps(2)
+      assert.closeTo(integrator.y[0], 0, 1e-4 )
+      assert.closeTo(integrator.y[1], 2, 1e-4)
+    })
 
-  });
+  })
 
   describe('euler integration with binding of extra data (' + dtype + ')', function() {
 
-    var integrator, f, y0;
+    var integrator, f, y0
 
     beforeEach(function() {
 
@@ -61,27 +61,27 @@ Object.keys(ctors).forEach(function(dtype) {
       }
 
       f = function(dydt, y) {
-        dydt[0] = -y[1] * this.scale;
-        dydt[1] =  y[0] * this.scale;
-      }.bind(data);
+        dydt[0] = -y[1] * this.scale
+        dydt[1] =  y[0] * this.scale
+      }.bind(data)
 
-      y0 = new ctor([1,0]);
+      y0 = new ctor([1,0])
 
-      integrator = euler( y0, f, 0, 1 );
-    });
+      integrator = euler( y0, f, 0, 1 )
+    })
 
     it("takes a single timestep",function() {
-      integrator.step();
-      assert.closeTo( integrator.y[0], 1, 1e-4 );
-      assert.closeTo( integrator.y[1], 2, 1e-4 );
-    });
+      integrator.step()
+      assert.closeTo( integrator.y[0], 1, 1e-4 )
+      assert.closeTo( integrator.y[1], 2, 1e-4 )
+    })
 
     it("takes multiple timesteps",function() {
-      integrator.steps(2);
-      assert.closeTo(integrator.y[0], -3, 1e-4 );
-      assert.closeTo(integrator.y[1],  4, 1e-4);
-    });
+      integrator.steps(2)
+      assert.closeTo(integrator.y[0], -3, 1e-4 )
+      assert.closeTo(integrator.y[1],  4, 1e-4)
+    })
 
-  });
+  })
 
 })
