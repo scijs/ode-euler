fb7f964a1669afc861680d271183891366e84607
Ricky Reusser <rsreusser@gmail.com>
2015-08-01 09:43:32 -0400

    Test convergence when transcendental in all variables

--- test/test.js
+++ test/test.js
@@ -124,6 +124,17 @@ Object.keys(ctors).forEach(function(dtype) {
 
     describe('convergence', function() {
 
+      it('local truncation error is ~ O(h^2) in time', function() {
+        // Integrate an exponential: dy/dt = -y  -->  y = exp(-t)
+        var result = richardson(function(h) {
+          var f = function(dydt, y) { dydt[0] = -y[0] }
+          return euler( new ctor([1]), f, 0, h ).step().y[0] - Math.exp(-h)
+        }, [0.06,0.01], { f: 0 } )
+
+        assert.closeTo( result.n, 2, 1e-2, 'n ~ 2' )
+      })
+
+
       it('local truncation error is order O(h^2)', function() {
         var result = richardson(function(h) {
 
@@ -154,6 +165,27 @@ Object.keys(ctors).forEach(function(dtype) {
 
         assert.closeTo( result.n, 1, 1e-1, 'n ~ 1' )
       })
+
+      it('total accumulated error is order O(h^1) in all variables', function() {
+
+        var result = richardson(function(h) {
+          // Integrate around a circle at an accelerating rate
+          var f = function(dydt, y, t) {
+            var s =  Math.sin(t * Math.PI) * Math.PI / 2
+            dydt[0] = -y[1]* 2 * Math.PI * s
+            dydt[1] =  y[0]* 2 * Math.PI * s
+          }
+          var i = euler( new ctor([1,0]), f, 0, h ).steps( Math.floor(1/h+0.5))
+
+          // Return the distance from the expected endpoint:
+          return Math.sqrt( Math.pow(i.y[0]-1,2) + Math.pow(i.y[1],2) )
+
+        }, 0.001, { f: 0 } )
+
+        assert.closeTo( result.n, 1, 1e-2, 'n ~ 1' )
+      })
+
+
 
     })
 
