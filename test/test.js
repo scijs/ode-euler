833823e9b7caf8f40aa06ee708eef854f9ab1ec1
Ricky Reusser <rsreusser@gmail.com>
2015-08-01 08:49:33 -0400

    Lock down more behavior with tests

--- test/test.js
+++ test/test.js
@@ -1,13 +1,14 @@
 'use strict'
 
 var euler = require('../lib')
-var assert = require('chai').assert
+  , assert = require('chai').assert
+  , richardson = require('richardson-extrapolation')
 
 
 var ctors = {
   'float32': Float32Array,
   'float64': Float64Array,
-  'array': function(){ return Array.apply(this,arguments[0]) }
+  'array': function(){ return arguments[0] }
 }
 
 Object.keys(ctors).forEach(function(dtype) {
@@ -15,71 +16,145 @@ Object.keys(ctors).forEach(function(dtype) {
 
   describe('euler integration (' + dtype + ')', function() {
 
-    var integrator, f, y0, t0
+    describe('integration of one variable', function() {
+      var integrator, f, y0, t0, n
 
-    beforeEach(function() {
-      f = function(dydt, y) {
-        dydt[0] = -y[1]
-        dydt[1] =  y[0]
-      }
+      beforeEach(function() {
+        f = function(dydt, y) { dydt[0] = -y[0] }
+        t0 = 1.5
+        y0 = new ctor([1])
+        n = 100
 
-      t0 = 1.5
-      y0 = new ctor([1,0])
+        integrator = euler( y0, f, t0, 1/n )
+      })
 
-      integrator = euler( y0, f, t0, 1 )
+      it('creates work arrays of the same size as the input',function() {
+        assert.equal( integrator._yp.constructor,  y0.constructor )
+      })
+
+      it('creates work arrays of the same type as the input',function() {
+        assert.equal( integrator._yp.length,  y0.length )
+      })
+
+      it('takes multiple timesteps',function() {
+        integrator.steps(n)
+        assert.closeTo(integrator.y[0], Math.exp(-1), 1e-2 )
+      })
     })
 
-    it("takes a single timestep",function() {
-      integrator.step()
-      assert.closeTo( integrator.y[0], 1, 1e-4 )
-      assert.closeTo( integrator.y[1], 1, 1e-4 )
+
+    describe('integration of two variables', function() {
+      var integrator, f, y0, t0
+
+      beforeEach(function() {
+        f = function(dydt, y) {
+          dydt[0] = -y[1]
+          dydt[1] =  y[0]
+        }
+
+        t0 = 1.5
+        y0 = new ctor([1,0])
+
+        integrator = euler( y0, f, t0, 1 )
+      })
+
+      it('creates work arrays of the same size as the input',function() {
+        assert.equal( integrator._yp.constructor,  y0.constructor )
+      })
+
+      it('creates work arrays of the same type as the input',function() {
+        assert.equal( integrator._yp.length,  y0.length )
+      })
+
+
+      it("takes a single timestep",function() {
+        integrator.step()
+        assert.closeTo( integrator.y[0], 1, 1e-4 )
+        assert.closeTo( integrator.y[1], 1, 1e-4 )
+      })
+
+      it("increments the time",function() {
+        integrator.step()
+        assert.closeTo( integrator.t, t0 + integrator.dt, 1e-4 )
+        integrator.step()
+        assert.closeTo( integrator.t, t0 + 2*integrator.dt, 1e-4 )
+      })
+
+      it("takes multiple timesteps",function() {
+        integrator.steps(2)
+        assert.closeTo(integrator.y[0], 0, 1e-4 )
+        assert.closeTo(integrator.y[1], 2, 1e-4)
+      })
+
     })
 
-    it("increments the time",function() {
-      integrator.step()
-      assert.closeTo( integrator.t, t0 + integrator.dt, 1e-4 )
-      integrator.step()
-      assert.closeTo( integrator.t, t0 + 2*integrator.dt, 1e-4 )
+    describe('euler integration with binding of extra data (' + dtype + ')', function() {
+
+      var integrator, f, y0
+
+      beforeEach(function() {
+
+        var data = {
+          scale: 2
+        }
+
+        f = function(dydt, y) {
+          dydt[0] = -y[1] * this.scale
+          dydt[1] =  y[0] * this.scale
+        }.bind(data)
+
+        y0 = new ctor([1,0])
+
+        integrator = euler( y0, f, 0, 1 )
+      })
+
+      it("takes a single timestep",function() {
+        integrator.step()
+        assert.closeTo( integrator.y[0], 1, 1e-4 )
+        assert.closeTo( integrator.y[1], 2, 1e-4 )
+      })
+
+      it("takes multiple timesteps",function() {
+        integrator.steps(2)
+        assert.closeTo(integrator.y[0], -3, 1e-4 )
+        assert.closeTo(integrator.y[1],  4, 1e-4)
+      })
+
     })
 
-    it("takes multiple timesteps",function() {
-      integrator.steps(2)
-      assert.closeTo(integrator.y[0], 0, 1e-4 )
-      assert.closeTo(integrator.y[1], 2, 1e-4)
-    })
+    describe('convergence', function() {
 
-  })
+      it('location truncation error is order O(h^2)', function() {
+        var result = richardson(function(h) {
 
-  describe('euler integration with binding of extra data (' + dtype + ')', function() {
+          // Integrate along a sector of a circle:
+          var f = function(dydt, y) { dydt[0] = -y[1]; dydt[1] =  y[0] }
+          var i = euler( new ctor([1,0]), f, 0, h ).step()
 
-    var integrator, f, y0
+          // Return the distance from the expected endpoint:
+          return Math.sqrt( Math.pow(i.y[0]-Math.cos(h),2) + Math.pow(i.y[1]-Math.sin(h),2) )
 
-    beforeEach(function() {
+        }, 2*Math.PI/100, { f: 0 } )
 
-      var data = {
-        scale: 2
-      }
+        assert.closeTo( result.n, 2, 1e-2, 'n ~ 2' )
+      })
 
-      f = function(dydt, y) {
-        dydt[0] = -y[1] * this.scale
-        dydt[1] =  y[0] * this.scale
-      }.bind(data)
+      it('total accumulated error is order O(h^1)', function() {
 
-      y0 = new ctor([1,0])
+        var result = richardson(function(h) {
 
-      integrator = euler( y0, f, 0, 1 )
-    })
+          // Integrate around a circle with this step size:
+          var f = function(dydt, y) { dydt[0] = -y[1]; dydt[1] =  y[0] }
+          var i = euler( new ctor([1,0]), f, 0, h ).steps( Math.floor(2*Math.PI/h + 0.5) )
 
-    it("takes a single timestep",function() {
-      integrator.step()
-      assert.closeTo( integrator.y[0], 1, 1e-4 )
-      assert.closeTo( integrator.y[1], 2, 1e-4 )
-    })
+          // Return the distance from the expected endpoint:
+          return Math.sqrt( Math.pow(i.y[0]-1,2) + Math.pow(i.y[1],2) )
 
-    it("takes multiple timesteps",function() {
-      integrator.steps(2)
-      assert.closeTo(integrator.y[0], -3, 1e-4 )
-      assert.closeTo(integrator.y[1],  4, 1e-4)
+        }, 2*Math.PI/100, { f: 0 } )
+
+        assert.closeTo( result.n, 1, 1e-1, 'n ~ 1' )
+      })
+
     })
 
   })
